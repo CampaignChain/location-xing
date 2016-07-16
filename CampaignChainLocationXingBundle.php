07505dd918c671a6781e52a94fd1c61b66361026
Sandro Groganz <sandro@campaignchain.com>
2016-07-16 12:32:21 -0700

    CampaignChain/campaignchain#192 Subsume all app-wide parameters under campaignchain_core using tree builder

--- CampaignChainLocationXingBundle.php
+++ CampaignChainLocationXingBundle.php
@@ -2,8 +2,13 @@
 
 namespace CampaignChain\Location\XingBundle;
 
+use CampaignChain\Location\XingBundle\DependencyInjection\CampaignChainLocationXingExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class CampaignChainLocationXingBundle extends Bundle
 {
+    public function getContainerExtension()
+    {
+        return new CampaignChainLocationXingExtension();
+    }
 }
